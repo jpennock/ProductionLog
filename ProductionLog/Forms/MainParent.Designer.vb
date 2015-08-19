fccc1703a2e787545b2d370608e3934f79080b26
Joe Pennock <joseph.pennock@gmail.com>
2015-08-19 14:11:32 -0500

    Starting the addition of the file upload portion of the new collateral log
    The super long title of this change should let you know what's going on

--- ProductionLog/Forms/MainParent.Designer.vb
+++ ProductionLog/Forms/MainParent.Designer.vb
@@ -23,21 +23,95 @@ Partial Class MainParent
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainParent))
+        Me.FormSkin1 = New ProductionLog.FormSkin()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.FileButton = New ProductionLog.FlatButton()
+        Me.FormSkin1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'FormSkin1
+        '
+        Me.FormSkin1.BackColor = System.Drawing.Color.White
+        Me.FormSkin1.BaseColor = System.Drawing.Color.Gray
+        Me.FormSkin1.BorderColor = System.Drawing.Color.FromArgb(CType(CType(53, Byte), Integer), CType(CType(58, Byte), Integer), CType(CType(60, Byte), Integer))
+        Me.FormSkin1.Controls.Add(Me.FileButton)
+        Me.FormSkin1.Controls.Add(Me.ComboBox1)
+        Me.FormSkin1.Controls.Add(Me.CheckedListBox1)
+        Me.FormSkin1.Controls.Add(Me.DataGridView1)
+        Me.FormSkin1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FormSkin1.FlatColor = System.Drawing.Color.SteelBlue
+        Me.FormSkin1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FormSkin1.HeaderColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.FormSkin1.HeaderMaximize = False
+        Me.FormSkin1.Location = New System.Drawing.Point(0, 0)
+        Me.FormSkin1.Name = "FormSkin1"
+        Me.FormSkin1.Size = New System.Drawing.Size(960, 659)
+        Me.FormSkin1.TabIndex = 1
+        Me.FormSkin1.Text = "VIN Uploader"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(3, 53)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(696, 603)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'CheckedListBox1
+        '
+        Me.CheckedListBox1.FormattingEnabled = True
+        Me.CheckedListBox1.Location = New System.Drawing.Point(731, 166)
+        Me.CheckedListBox1.Name = "CheckedListBox1"
+        Me.CheckedListBox1.Size = New System.Drawing.Size(204, 100)
+        Me.CheckedListBox1.TabIndex = 1
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(731, 53)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(204, 29)
+        Me.ComboBox1.TabIndex = 2
+        '
+        'FileButton
+        '
+        Me.FileButton.BackColor = System.Drawing.Color.Transparent
+        Me.FileButton.BaseColor = System.Drawing.Color.SteelBlue
+        Me.FileButton.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FileButton.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FileButton.Location = New System.Drawing.Point(774, 108)
+        Me.FileButton.Name = "FileButton"
+        Me.FileButton.Rounded = False
+        Me.FileButton.Size = New System.Drawing.Size(106, 32)
+        Me.FileButton.TabIndex = 3
+        Me.FileButton.Text = "Select File"
+        Me.FileButton.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
         '
         'MainParent
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(849, 705)
+        Me.ClientSize = New System.Drawing.Size(960, 659)
+        Me.Controls.Add(Me.FormSkin1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.IsMdiContainer = True
         Me.Name = "MainParent"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Primalend Production Log"
         Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.FormSkin1.ResumeLayout(False)
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents FormSkin1 As ProductionLog.FormSkin
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents FileButton As ProductionLog.FlatButton
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents CheckedListBox1 As System.Windows.Forms.CheckedListBox
 
 End Class
--- ProductionLog/Forms/MainParent.vb
+++ ProductionLog/Forms/MainParent.vb
@@ -1,7 +1,7 @@
 ﻿Public Class MainParent
 
     Private Sub MainParent_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dashboard.Show()
+
     End Sub
 
     Private Sub FormSkin1_Click(sender As Object, e As EventArgs)
